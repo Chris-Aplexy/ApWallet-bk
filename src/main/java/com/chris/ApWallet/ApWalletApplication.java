8ae84594acf0e489a8d1db9396dd4ef0bfc100a2
Chris <96912309+Chris-Mwesigwa@users.noreply.github.com>
2023-04-06 15:22:23 +0300

    commit one

---
+++ src/main/java/com/chris/ApWallet/ApWalletApplication.java
@@ -0,0 +1,13 @@
+package com.chris.ApWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApWalletApplication.class, args);
+	}
+
+}
